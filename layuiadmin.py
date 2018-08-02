41e993918e9dda9a9b2712b69c19e69cfb90ce41
zhibushiwo <zhibushiwo@163.com>
2018-08-02 22:02:14 +0800

    更新一下

--- layuiadmin.py
+++ layuiadmin.py
@@ -38,24 +38,27 @@ def get_me(list,name):
             get_me(l.get('list'),name+'/'+l.get('name'))
         else:
             if name!=None:  
-                url=name+'/'+ l.get('name')         
-                reg=re.compile("(?=/)")
-                if len(reg.findall(url))>1:
-                    test(url,l.get('title'))
-                    continue
+                if l.get('jump')!=None:
+                     test(l.get('jump'),l.get('title'))
                 else :
-                    test(url+'/'+'index',l.get('title'))
+                    url=name+'/'+ l.get('name')         
+                    reg=re.compile("(?=/)")
+                    if len(reg.findall(url))>1:
+                        test(url,l.get('title'))
+                        continue
+                    else :
+                        test(url+'/'+'index',l.get('title'))
 
 def test(menu,title):
     url='http://www.layui.com/admin/pro/dist/views/'+ menu+'.html?v=1.0.0'
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"}
     r=requests.get(url,headers=headers)
-    r.encoding = 'utf-8'
-    soup2 = BeautifulSoup(r.text, 'html.parser')
-    t = soup2.title.text
-    if t =='好干净的404 - layui':
-        print(title)
-        return
+    # r.encoding = 'utf-8'
+    # soup2 = BeautifulSoup(r.text, 'html.parser')
+    # t = soup2.title.text
+    # if t =='好干净的404 - layui':
+    #     print(title)
+    #     return
     create_dir('layuiadmin')
     with open('layuiadmin/{}'.format(title+'.html'),'wb') as f:
         f.write(r.content)
